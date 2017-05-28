0527f129e065b445d66ef909a6c06af2bc96eaec
Matthew Langford <matthew.langford96@gmail.com>
2017-05-28 12:02:29 +1000

    Moved live search to seperate js file, push for testing.

--- search.php
+++ search.php
@@ -74,33 +74,7 @@
             echo "</div>";
                 ?>
        
-		<script>
-function showResult(str) {
-  if (str.length==0) { 
-    document.getElementById("livesearch").innerHTML="";
-    document.getElementById("livesearch").style.border="0px";
-    return;
-  }
-  if (window.XMLHttpRequest) {
-    // code for IE7+, Firefox, Chrome, Opera, Safari
-    xmlhttp=new XMLHttpRequest();
-  } else {  // code for IE6, IE5
-    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  }
-  xmlhttp.onreadystatechange=function() {
-    if (this.readyState==4 && this.status==200) {
-      document.getElementById("livesearch").innerHTML=this.responseText;
-      document.getElementById("livesearch").style.border="1px solid #A5ACB2";
-    }
-  }
-  xmlhttp.open("GET","live_search.php?q="+str,true);
-  xmlhttp.send();
-}
-function selectDrug(h) {
-    document.getElementById("main_search").value = h;
-    document.getElementById("livesearch").style.display = 'none';
-}
-</script>
+	<script src = "js/live_search.js" type = "application/javascript"> </script>
     </BODY>   
 </HTML>
 
