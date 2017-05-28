e2c59e320ed806061bfb7ccb30b0d21856a851f8
Matthew Langford <matthew.langford96@gmail.com>
2017-05-28 12:18:49 +1000

    Switched search and results pages, added live search to search page. Push for testing.

--- index.php
+++ index.php
@@ -1,15 +1,9 @@
 <!DOCTYPE HTML>
     <HEAD>
-
         <title>RBWH</title>
         <link rel = "stylesheet" type = "text/css" href = "style/index.css">
-
-        <?php
-            session_start(); 
-        ?>
-
+        <?php session_start(); ?>
     </HEAD>
-
 
     <BODY>
         <div class = "top_pane">
@@ -21,15 +15,13 @@
            <br>
            <a href="create_user.php" target="_blank">Forgot your password or don't have an account?</a>
            <br>
-           <form class = "login_form" action = "results.php" method = "POST">
+           <form class = "login_form" action = "search.php" method = "POST">
                 <input class = "username" type = "text" 
                         name = "username" placeholder = "username">
                 <br>
-
                 <input class = "password" type = "password" 
                         name = "password" placeholder = "password">
                 <br>
-
                 <input class = "submit"  type = "submit"
                         value = "SUBMIT" > 
             </form>
@@ -38,7 +30,6 @@
                     echo "<p> Incorrect username or password </p>";
                 }
             ?>
- 
         </div>
     </BODY>
 
--- results.php
+++ results.php
@@ -1,23 +1,34 @@
 <!DOCTYPE HTML>
-    <HEAD>
-        <title>Search</title>
-        <link rel = "stylesheet" type = "text/css" href = "style/results.css">
-        <?php
-            session_start(); 
-            $_SESSION['username'] = $_POST['username'];
-            $_SESSION['password'] = $_POST['password'];
-            $connection = mysql_connect("localhost", "MLangford", "Redline66");
+    <HEAD>   
+    <title>Search</title>
+        <link rel = "stylesheet" type = "text/css" href = "style/search.css">
+        <?php session_start();
+            $connection = mysql_connect("localhost", 'MLangford', 'Redline66') or die('error');
+            $db_selected = mysql_select_db("MilkBank", $connection) or die ("error");
 
             if (!$connection) {
                 echo "connection failed";
-                $_SESSION['pass_error'] = 1;
+                $_POST['pass_error'] = 1;
                 header('Location: https://infs3202-gzhlr.uqcloud.net/');
             }
-             else if(isset($_SESSION['pass_error'])) {
-                unset($_SESSION['pass_error']);
-            }
-            $db_selected = mysql_select_db("MilkBank", $connection) or die ("error");
-            $username = $_SESSION['username'];           
+         ?> 
+    </HEAD>
+
+    <BODY>
+        <div class = "top_pane">
+            <h1 class="heading"> RBWH Milk Bank <br> Drug Guide </h1>
+        </div>
+ 
+        <div class = "search_pane">
+            <form action = "search.php" method = "POST">
+                <input id = main_search class = "search_in" type="text" name="input_text" 
+                        placeholder = "Enter Keywords" onkeyup="showResult(this.value)">
+                <div id="livesearch"></div>
+                <input class = "submit" type="submit" value = "Search">
+            </form>
+        </div>
+        <?php
+           $username = $_SESSION['username'];           
             $password = $_SESSION['password'];
 
             $result = mysql_query("SELECT * FROM USERS WHERE USERNAME='$username'");
@@ -26,28 +37,42 @@
             if (password_verify($password, $hash)) {
                 //passwords match 
                 if (isset($_SESSION['bad_password'])) {
-                    unset($_SESSION['bad_password']); 
-                }
+                    unset($_SESSION['bad_password']);
+                } 
             } else {
                 $_SESSION['bad_password'] = 1;
                 header('Location: https://infs3202-gzhlr.uqcloud.net/');
             }            
-        ?>
- 
-    </HEAD>
+            
+            $input_text = $_POST['input_text'];
+            $tokens = explode(' ', $input_text);
+            $output_text = implode("|", $tokens);
+            $numResults = 0;           
+            $result = mysql_query("SELECT * FROM Medication WHERE NAME REGEXP '$output_text' OR INGREDIENTS REGEXP '$output_text'");
+            echo "<div class = \"result_pane\">";
+                echo "<table class = \"results\"> <tr>";
+                    echo "<th>Name</th> <th>Dosage (mg)</th> <th>Half Life (hrs)</th>";
+                            echo "<th>Stand Down (hrs)</th> <th>Ingredioents</th> <th>Approval</th></tr>";
+             
+                    while ($row = mysql_fetch_array($result)) {
+                        echo "<tr> <td>".$row['0']."</td>";
+                        echo "<td>".$row['1']."</td>";
+                        echo "<td>".$row['2']."</td>";               
+                        echo "<td>".$row['3']."</td>";
+                        echo "<td>".$row['4']."</td>";
+                        echo "<td>".$row['5']."</td>"; 
+                        echo "<td> <input type = \"checkbox\" name = \"drug$numResults\" value =".$row['0']." form = \"approval\" />  </tr>";
+                        $numResults++;          
+                    }           
+            echo "</table>";
+            echo "<form action = \"approval.php\" method = \"POST\" id = \"approval\">"; 
+            echo "<input class = \"submit\" type = \"submit\" value = \"submit for approval\" form = \"approval\" />"; 
+            echo "</form>";
 
-    <BODY>
-        <div class = "top_pane">
-            <h1 class="heading"> RBWH Milk Bank <br> Drug Guide </h1>
-        </div>
-
-        <div class = "search_pane">
-            <form action = "search.php" method = "POST">
-                <input class = "search_in" type = "text" name = "input_text" 
-                        placeholder = "Enter keywords">
-                <input class = "submit" type = "submit" value = "Search">
-            </form>
-        </div>
+            echo "</div>";
+                ?>
+       
+	<script src = "js/live_search.js" type = "application/javascript"> </script>
     </BODY>   
 </HTML>
 
